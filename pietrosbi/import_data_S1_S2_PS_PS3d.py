2929842d9622ab2f449023f4557751a8a3facbd1
pietro-alba <146743448+pietro-alba@users.noreply.github.com>
2025-02-12 12:14:48 +0100

    Update of all the functions

--- pietrosbi/import_data_S1_S2_PS_PS3d.py
+++ pietrosbi/import_data_S1_S2_PS_PS3d.py
@@ -2,8 +2,8 @@
 
 import glob
 import numpy as np
-import torch
 import pywt
+import os
 
 def import_data_test(path):
     '''
@@ -171,74 +171,8 @@ def l1_l2_summary_S2(S2, n_pixels, wavelet_type, l1, l2):
     else:
         return 0
 
-
-def norm_data(data_PS, data_S1, data_S2):
-    '''
-    This function normalizes S1 and S2 with PS and S1 respectively
-
-    Inputs:
-        - data_PS: array for PS
-        - data_S1: array for S1
-        - data_S2: array for S2
-
-    Outputs:
-        - data_S1_new: normalized arrau for S1
-        - data_S2_new: normalized arrau for S2
-    '''
     
-    # Nprmalization of S1
-    data_S1_new = data_S1 / np.sqrt(data_PS)
-
-    # Reversing S1, becasue S2 is define in the opposite way as S1 (look at the class Pietro_Wavelet_Transorms())
-    data_S1_rev = data_S1_new[:,:,::-1]
-
-    # Creating a new axis to match the size of the shape of S2
-    data_S1_rev = data_S1_rev[:,:,:,np.newaxis] 
-
-    # Normalization of S2
-    data_S2_new = data_S2 / data_S1_rev 
-
-    # Setting nan values to 0
-    data_S1_new[np.isnan(data_S1_new)] = 0
-    data_S2_new[np.isnan(data_S2_new)] = 0
-
-    return data_S1_new, data_S2_new
-
-
-def norm_obs_data(data_obs_PS, data_obs_S1, data_obs_S2):
-    '''
-    This function normalizes S1 and S2 with PS and S1 respectively
-
-    Inputs:
-        - data_PS: array for PS
-        - data_S1: array for S1
-        - data_S2: array for S2
-
-    Outputs:
-        - data_S1_new: normalized arrau for S1
-        - data_S2_new: normalized arrau for S2
-    '''
-
-    # Nprmalization of S1
-    data_obs_S1_new = data_obs_S1 / np.sqrt(data_obs_PS)
-
-    # Reversing S1, becasue S2 is define in the opposite way as S1 (look at the class Pietro_Wavelet_Transorms())
-    data_obs_S1_rev = data_obs_S1_new[:,::-1]
-
-    # Creating a new axis to match the size of the shape of S2
-    data_obs_S1_rev = data_obs_S1_rev[:,:,np.newaxis] 
-
-    # Normalization of S2
-    data_obs_S2_new = data_obs_S2 / data_obs_S1_rev 
-
-    # Setting nan values to 0
-    data_obs_S1_new[np.isnan(data_obs_S1_new)] = 0
-    data_obs_S2_new[np.isnan(data_obs_S2_new)] = 0
-
-    return data_obs_S1_new, data_obs_S2_new
-
-    
-def calculate_l1l2(data_PS, data_S1, data_S2, n_pixels, wavelet_type, l1, l2):
+def calculate_l1l2(data_PS, data_S1, data_S2, n_pixels, wavelet_type, l1, l2, save_path = ''):
     '''
     This function calculates l1 and l2 summaries and reshape the arrays.
     Reshaping is done in such a way to change the shape 
@@ -259,23 +193,26 @@ def calculate_l1l2(data_PS, data_S1, data_S2, n_pixels, wavelet_type, l1, l2):
         - l1l2_summary_S2: l1 and l2 summaries of S2
     '''
 
-    # Normalization of S1 and S2
-    data_S1_new, data_S2_new = norm_data(data_PS, data_S1, data_S2)
-
     # Calculating the l1l2 summaries
     l1l2_summary_PS = np.array([list(l1_l2_summary(data_PS[i], n_pixels, wavelet_type, l1, l2)) for i in range(len(data_PS))])
-    l1l2_summary_S1 = np.array([list(l1_l2_summary(data_S1_new[i], n_pixels, wavelet_type, l1, l2)) for i in range(len(data_S1_new))])
-    l1l2_summary_S2 = np.array([list(l1_l2_summary_S2(data_S2_new[i], n_pixels, wavelet_type, l1, l2)) for i in range(len(data_S2_new))])   
+    l1l2_summary_S1 = np.array([list(l1_l2_summary(data_S1[i], n_pixels, wavelet_type, l1, l2)) for i in range(len(data_S1))])
+    l1l2_summary_S2 = np.array([list(l1_l2_summary_S2(data_S2[i], n_pixels, wavelet_type, l1, l2)) for i in range(len(data_S2))])   
 
     # Reshaping the summaries
     l1l2_summary_PS = np.reshape(l1l2_summary_PS, (l1l2_summary_PS.shape[0], -1))
     l1l2_summary_S1 = np.reshape(l1l2_summary_S1, (l1l2_summary_S1.shape[0], -1))
     l1l2_summary_S2 = np.reshape(l1l2_summary_S2, (l1l2_summary_S2.shape[0], -1))
 
+    if save_path:
+        np.save(os.path.join(save_path, 'l1l2_summary_PS.npy'), np.float32(l1l2_summary_PS))
+        np.save(os.path.join(save_path, 'l1l2_summary_S1.npy'), np.float32(l1l2_summary_S1))
+        np.save(os.path.join(save_path, 'l1l2_summary_S2.npy'), np.float32(l1l2_summary_S2))
+
+
     return l1l2_summary_PS, l1l2_summary_S1, l1l2_summary_S2
 
 
-def calculate_obs_l1l2(data_obs_PS, data_obs_S1, data_obs_S2, n_pixels, wavelet_type, l1, l2):
+def calculate_obs_l1l2(data_obs_PS, data_obs_S1, data_obs_S2, n_pixels, wavelet_type, l1, l2, save_path = ''):
     '''
     This function calculates l1 and l2 summaries and reshape the arrays for "observed" data.
     Reshaping is done in such a way to change the shape 
@@ -296,124 +233,24 @@ def calculate_obs_l1l2(data_obs_PS, data_obs_S1, data_obs_S2, n_pixels, wavelet_
         - l1l2_summary_obs_S2: l1 and l2 summaries of "observed" S2
     '''
 
-    # Normalization of S1 and S2
-    data_obs_S1_new, data_obs_S2_new = norm_obs_data(data_obs_PS, data_obs_S1, data_obs_S2)
-
     # Calculating the l1l2 summaries
     l1l2_summary_obs_PS = np.array([list(l1_l2_summary(data_obs_PS, n_pixels, wavelet_type, l1, l2))])
-    l1l2_summary_obs_S1 = np.array([list(l1_l2_summary(data_obs_S1_new, n_pixels, wavelet_type, l1, l2))])
-    l1l2_summary_obs_S2 = np.array([list(l1_l2_summary_S2(data_obs_S2_new, n_pixels, wavelet_type, l1, l2))])
+    l1l2_summary_obs_S1 = np.array([list(l1_l2_summary(data_obs_S1, n_pixels, wavelet_type, l1, l2))])
+    l1l2_summary_obs_S2 = np.array([list(l1_l2_summary_S2(data_obs_S2, n_pixels, wavelet_type, l1, l2))])
 
     # Reshaping the summaries
     l1l2_summary_obs_PS = l1l2_summary_obs_PS.flatten()
     l1l2_summary_obs_S1 = l1l2_summary_obs_S1.flatten()
     l1l2_summary_obs_S2 = l1l2_summary_obs_S2.flatten()
 
+    if save_path:
+        np.save(os.path.join(save_path, 'l1l2_summary_obs_PS.npy'), np.float32(l1l2_summary_obs_PS))
+        np.save(os.path.join(save_path, 'l1l2_summary_obs_S1.npy'), np.float32(l1l2_summary_obs_S1))
+        np.save(os.path.join(save_path, 'l1l2_summary_obs_S2.npy'), np.float32(l1l2_summary_obs_S2))
+
     return l1l2_summary_obs_PS, l1l2_summary_obs_S1, l1l2_summary_obs_S2
 
 
-def torchify(array):
-    '''
-    This function converst a numpy array to torch.tensor
-    whose values are numnpy.float32 (instead of numpy.float64)
-    The NDE wants torch.tensors whose values are float32 and not float64
-
-    Inputs:
-        - array: numpy array
-
-    Outputs:
-        - a torch.tensor with float32 values
-
-    '''
-    
-    return torch.from_numpy(np.float32(array))
 
 
-def l1l2_and_torchify_data(data_PS, data_S1, data_S2, data_PS3d, param_list, n_pixels, wavelet_type, l1, l2):
-    '''
-    This function calls the function to calculate the l1 and l2 summaries
-    of PS, S1 and S2 and converts them to torch.tensor.
-    It also convert PS3D and the paramter list to torch.tensors.
 
-    Inputs:
-        - data_PS: array for PS
-        - data_S1: array for S1
-        - data_S2: array for S2
-        - data_PS3d: array for PS3d
-        - param_list: list of parameters
-        - n_pixels: number of pixels of the coeval cube
-        - wavelet_type: wavelet to be used for the wavelet scattering transform
-        - l1: boolean value if you want to calculate l1
-        - l2: boolean value if you want to calculate l2
-
-    Outputs:
-        - l1l2_summary_PS: l1 and l2 summaries of PS
-        - l1l2_summary_S1: l1 and l2 summaries of S1
-        - l1l2_summary_S2: l1 and l2 summaries of S2
-        - data_PS3d: torch.tensor for PS3d
-        - param_list: torch.tensor of parameters
-    '''
-
-    # Calculating the l1 and l2 summaries for PS, S1 and S2
-    l1l2_summary_PS, l1l2_summary_S1, l1l2_summary_S2 = calculate_l1l2(data_PS, 
-                                                                       data_S1, 
-                                                                       data_S2, 
-                                                                       n_pixels, 
-                                                                       wavelet_type,
-                                                                       l1, 
-                                                                       l2
-                                                                      )
-
-    # Converting the summaries, PS3d and parameter list to torch.tensors
-    l1l2_summary_PS = torchify(l1l2_summary_PS)
-    l1l2_summary_S1 = torchify(l1l2_summary_S1)
-    l1l2_summary_S2 = torchify(l1l2_summary_S2)
-    data_PS3d = torchify(data_PS3d)
-    param_list = torchify(param_list)
-
-    return l1l2_summary_PS, l1l2_summary_S1, l1l2_summary_S2, data_PS3d, param_list
-
-
-def l1l2_and_torchify_obs_data(data_obs_PS, data_obs_S1, data_obs_S2, data_obs_PS3d, param_true, n_pixels, wavelet_type, l1, l2):
-    '''
-    This function calls the function to calculate the l1 and l2 summaries
-    of PS, S1 and S2 and converts them to torch.tensor.
-    It also convert PS3D and the paramter list to torch.tensors.
-
-    Inputs:
-        - data_obs_PS: array for "observed" PS
-        - data_obs_S1: array for "observed" S1
-        - data_obs_S2: array for "observed" S2
-        - data_obs_PS3d: array for "observed" PS3d
-        - param_true: true parameters for the observation
-        - n_pixels: number of pixels of the coeval cube
-        - wavelet_type: wavelet to be used for the wavelet scattering transform
-        - l1: boolean value if you want to calculate l1
-        - l2: boolean value if you want to calculate l2
-
-    Outputs:
-        - l1l2_summary_obs_PS: l1 and l2 summaries of "observed" PS
-        - l1l2_summary_obs_S1: l1 and l2 summaries of "observed" S1
-        - l1l2_summary_obs_S2: l1 and l2 summaries of "observed" S2
-        - data_obs_PS3d: torch.tensor for PS3d
-        - param_true: torch.tensor of the true parameters
-    '''
-
-    # Calculating the l1 and l2 summaries for PS, S1 and S2
-    l1l2_summary_obs_PS, l1l2_summary_obs_S1, l1l2_summary_obs_S2 = calculate_obs_l1l2(data_obs_PS, 
-                                                                                       data_obs_S1, 
-                                                                                       data_obs_S2, 
-                                                                                       n_pixels, 
-                                                                                       wavelet_type,
-                                                                                       l1,
-                                                                                       l2
-                                                                                      )
-
-    # Converting the summaries, PS3d and true parameters to torch.tensors
-    l1l2_summary_obs_PS = torchify(l1l2_summary_obs_PS)
-    l1l2_summary_obs_S1 = torchify(l1l2_summary_obs_S1)
-    l1l2_summary_obs_S2 = torchify(l1l2_summary_obs_S2)
-    data_obs_PS3d = torchify(data_obs_PS3d)
-    param_true = torchify(param_true)
-
-    return l1l2_summary_obs_PS, l1l2_summary_obs_S1, l1l2_summary_obs_S2, data_obs_PS3d, param_true
